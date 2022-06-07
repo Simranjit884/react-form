177b9d718420a9c208585f40433689d2de75f7b5
samy884 <ssgill884@gmail.com>
2022-06-07 10:40:21 +0530

    using ref for name input

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,22 @@
+import { useRef } from "react";
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+
+  const onNameInputChangeHandler = () => {
+    const inputRefValue = nameInputRef.current.value;
+    console.log(inputRefValue);
+  }
+
+  const onFormSubmitHandler = (event) => {
+    event.preventDefault();
+  }
+
   return (
-    <form>
+    <form onSubmit={onFormSubmitHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input ref={nameInputRef} type='text' id='name' onChange={onNameInputChangeHandler}/>
       </div>
       <div className="form-actions">
         <button>Submit</button>
